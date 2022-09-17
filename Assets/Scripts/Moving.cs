00dd2968d57bbe33ddc5b5bce020679b0824d6f0
AlexeyVashurin <a.s.vashurin@gmail.com>
2022-09-17 12:50:26 +0400

    Add Train Model, add rotation for train

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -15,23 +15,35 @@ public class Moving : MonoBehaviour
     {
         transform.position = _path.GetStartPosition().transform.position;
         pointInPath = _path.GetNextPathPoint();
-        if (pointInPath == null)
-            return;
     }
 
-    public void Update()
+    public void FixedUpdate()
     {
         if (pointInPath == null)
-         return; 
+            return;
 
+        TrainMoving();
+        TrainRotation();
+        CheckLastPathPoint();
+    }
+
+    void TrainMoving()
+    {
         transform.position = Vector3.MoveTowards(transform.position, pointInPath.transform.position, Time.deltaTime * _speed);
+    }
 
+    void TrainRotation()
+    {
+        Vector3 direction = pointInPath.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 10 * Time.deltaTime);   
+    }
+
+    void CheckLastPathPoint()
+    {
         var distance = (transform.position - pointInPath.transform.position).sqrMagnitude;
-        
         if (distance < _maxDistance * _maxDistance)
-        {
             pointInPath = _path.GetNextPathPoint();
-        }
-
+        
     }
 }
